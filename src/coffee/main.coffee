d1fb60c07051dd6ae488173b45102d260d563d96
giginet <giginet.net@gmail.com>
2013-10-30 10:50:06 +0900

    Parse repository link

--- src/coffee/main.coffee
+++ src/coffee/main.coffee
@@ -1,1 +1,8 @@
-console.log("hello")
+$ ->
+  $("a").on('mouseover', (e) ->
+    url = $(@).attr('href')
+    URL_REGEX = '^(https:\/\/github.com)?\/([a-zA-Z0-9_\.\-]+)\/([a-zA-Z0-9_\.\-]+)\/?$'
+    if url.match(URL_REGEX)
+      user = RegExp.$2
+      repository = RegExp.$3
+  )
--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,14 @@
 (function() {
-  console.log("hello");
+  $(function() {
+    return $("a").on('mouseover', function(e) {
+      var URL_REGEX, repository, url, user;
+      url = $(this).attr('href');
+      URL_REGEX = '^(https:\/\/github.com)?\/([a-zA-Z0-9_\.\-]+)\/([a-zA-Z0-9_\.\-]+)\/?$';
+      if (url.match(URL_REGEX)) {
+        user = RegExp.$2;
+        return repository = RegExp.$3;
+      }
+    });
+  });
 
 }).call(this);
