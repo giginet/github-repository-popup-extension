243c818caf26ab387fa768ee1fc55e7babc38e10
giginet <giginet.net@gmail.com>
2013-10-30 11:39:50 +0900

    Implement color code loader.

--- src/coffee/main.coffee
+++ src/coffee/main.coffee
@@ -1,8 +1,26 @@
 $ ->
+
+  URL_REGEX = '^(https:\/\/github.com)?\/([a-zA-Z0-9_\.\-]+)\/([a-zA-Z0-9_\.\-]+)\/?$'
+  API_BASE = 'https://api.github.com'
+  colors = {}
+
+  $.getJSON(chrome.extension.getURL('/resources/colors.json'), (json) ->
+    colors = json
+  )
+
   $("a").on('mouseover', (e) ->
     url = $(@).attr('href')
-    URL_REGEX = '^(https:\/\/github.com)?\/([a-zA-Z0-9_\.\-]+)\/([a-zA-Z0-9_\.\-]+)\/?$'
     if url.match(URL_REGEX)
       user = RegExp.$2
       repository = RegExp.$3
+      
+      $.get("#{API_BASE}/repos/#{user}/#{repository}/languages", (languages) ->
+        sorted = ([key, value] for key, value of languages).sort( (a, b) ->
+          a[1] < b[1]
+        )
+        languages = (language[0] for language in sorted)
+        major = languages[0]
+        colorCode = colors[major]
+        console.log colorCode
+      )
   )
--- src/js/main.js
+++ src/js/main.js
@@ -1,12 +1,44 @@
 (function() {
   $(function() {
+    var API_BASE, URL_REGEX, colors;
+    URL_REGEX = '^(https:\/\/github.com)?\/([a-zA-Z0-9_\.\-]+)\/([a-zA-Z0-9_\.\-]+)\/?$';
+    API_BASE = 'https://api.github.com';
+    colors = {};
+    $.getJSON(chrome.extension.getURL('/resources/colors.json'), function(json) {
+      return colors = json;
+    });
     return $("a").on('mouseover', function(e) {
-      var URL_REGEX, repository, url, user;
+      var repository, url, user;
       url = $(this).attr('href');
-      URL_REGEX = '^(https:\/\/github.com)?\/([a-zA-Z0-9_\.\-]+)\/([a-zA-Z0-9_\.\-]+)\/?$';
       if (url.match(URL_REGEX)) {
         user = RegExp.$2;
-        return repository = RegExp.$3;
+        repository = RegExp.$3;
+        return $.get("" + API_BASE + "/repos/" + user + "/" + repository + "/languages", function(languages) {
+          var colorCode, key, language, major, sorted, value;
+          sorted = ((function() {
+            var _results;
+            _results = [];
+            for (key in languages) {
+              value = languages[key];
+              _results.push([key, value]);
+            }
+            return _results;
+          })()).sort(function(a, b) {
+            return a[1] < b[1];
+          });
+          languages = (function() {
+            var _i, _len, _results;
+            _results = [];
+            for (_i = 0, _len = sorted.length; _i < _len; _i++) {
+              language = sorted[_i];
+              _results.push(language[0]);
+            }
+            return _results;
+          })();
+          major = languages[0];
+          colorCode = colors[major];
+          return console.log(colorCode);
+        });
       }
     });
   });
